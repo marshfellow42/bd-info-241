e46d428e5d5bc7b0d58cab611f46c3e10b8886d6
marshfellow42 <123888466+marshfellow42@users.noreply.github.com>
2024-09-09 20:40:05 -0300

    Enviar a atividade 9

---
+++ atividades/atividade-09/README.md
@@ -0,0 +1,70 @@
+# Atividade-09
+
+Usar as informações definidas na Avaliação-08 e o prompt da aula do dia 09-09-2024.
+
+Executar o programa Python no ambiente do Play with Docker incluindo a seguinte funcionalidade:
+
+Ler todos os registros da tabela `TB_ALUNOS` e mostrar o status de Aprovação. Usando a seguinte lógica:
+- Se o numero de faltas for maior ou igual a 20 setar o atributo Aprovado_SN com FALSO (REPROVADO);
+- Se a média Aritmética de N1 e N2 for < 6.0 setar o atributo Aprovado_SN com FALSO (REPROVADO);
+
+O código Python para rodar esse registro é esse:
+
+```python
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="myuser",
+  password="mypassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("""CREATE TABLE IF NOT EXISTS TB_ALUNOS (
+  id INT AUTO_INCREMENT NOT NULL,
+  nome TEXT,
+  nota_N1 INT,
+  nota_N2 INT,
+  media FLOAT,
+  faltas INT,
+  Aprovado_SN BOOLEAN,
+  PRIMARY KEY (id)
+);""")
+
+mycursor.executemany('INSERT INTO TB_ALUNOS (nome, nota_N1, nota_N2, faltas) VALUES (%s, %s, %s, %s);', [
+    ("Leandro", 8, 1, 20),
+    ("Ana Lívia", 10, 10, 0),
+    ("José Maia", 10, 2, 1),
+    ("Kelwin", 9, 0, 30)
+])
+
+mydb.commit()
+
+mycursor.execute("SELECT * FROM TB_ALUNOS")
+meuresultado = mycursor.fetchall()
+
+for item in meuresultado:
+    id, nome, nota_N1, nota_N2, media, faltas, aprovado_sn = item
+
+    media_parcial = (2 * nota_N1 + 3 * nota_N2) / 5
+
+    if media_parcial < 6.0 or faltas >= 20:
+        aprovado_sn = False
+    else:
+        aprovado_sn = True
+
+    mycursor.execute('UPDATE TB_ALUNOS SET Aprovado_SN = %s, media = %s WHERE id = %s;', (aprovado_sn, media_parcial, id))
+
+mydb.commit()
+
+mycursor.execute("SELECT * FROM TB_ALUNOS")
+meuresultado = mycursor.fetchall()
+
+for item in meuresultado:
+    print(item)
+
+mycursor.close()
+mydb.close()
+```
---
+++ atividades/atividade-09/rode_isso_no_shell.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+apk update
+
+apk upgrade
+
+cat <<EOF > docker-compose.yml
+version: '3.8'
+
+services:
+  mysql:
+    image: mysql:8.0
+    container_name: mysql_container
+    environment:
+      MYSQL_ROOT_PASSWORD: rootpassword
+      MYSQL_DATABASE: mydatabase
+      MYSQL_USER: myuser
+      MYSQL_PASSWORD: mypassword
+    volumes:
+      - mysql_data:/var/lib/mysql
+    ports:
+      - "3306:3306"
+
+  phpmyadmin:
+    image: phpmyadmin/phpmyadmin
+    container_name: phpmyadmin_container
+    environment:
+      PMA_HOST: mysql
+      PMA_PORT: 3306
+      PMA_USER: root
+      PMA_PASSWORD: rootpassword
+    ports:
+      - "8080:80"
+    depends_on:
+      - mysql
+
+volumes:
+  mysql_data:
+EOF
+
+docker login -u leanc -p arcoeflecha
+
+docker-compose up -d
+
+pip install mysql-connector-python
+
+echo
+
+show_progress() {
+    for i in {1..40}; do
+        echo -ne "Iniciando o Servidor MySQL.   \r"
+        sleep 0.5
+        echo -ne "Iniciando o Servidor MySQL..  \r"
+        sleep 0.5
+        echo -ne "Iniciando o Servidor MySQL... \r"
+        sleep 0.5
+        echo -ne "\r"
+    done
+}
+
+show_progress
+
+echo
+
+echo "O servidor MySQL foi iniciado!"
+
+echo
+
+cat <<EOF > arquivo.py
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="myuser",
+  password="mypassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("""CREATE TABLE IF NOT EXISTS TB_ALUNOS (
+  id INT AUTO_INCREMENT NOT NULL,
+  nome TEXT,
+  nota_N1 INT,
+  nota_N2 INT,
+  media FLOAT,
+  faltas INT,
+  Aprovado_SN BOOLEAN,
+  PRIMARY KEY (id)
+);""")
+
+mycursor.executemany('INSERT INTO TB_ALUNOS (nome, nota_N1, nota_N2, faltas) VALUES (%s, %s, %s, %s);', [
+    ("Leandro", 8, 1, 20),
+    ("Ana Lívia", 10, 10, 0),
+    ("José Maia", 10, 2, 1),
+    ("Kelwin", 9, 0, 30)
+])
+
+mydb.commit()
+
+mycursor.execute("SELECT * FROM TB_ALUNOS")
+meuresultado = mycursor.fetchall()
+
+for item in meuresultado:
+    id, nome, nota_N1, nota_N2, media, faltas, aprovado_sn = item
+
+    media_parcial = (2 * nota_N1 + 3 * nota_N2) / 5
+
+    if media_parcial < 6.0 or faltas >= 20:
+        aprovado_sn = False
+    else:
+        aprovado_sn = True
+
+    mycursor.execute('UPDATE TB_ALUNOS SET Aprovado_SN = %s, media = %s WHERE id = %s;', (aprovado_sn, media_parcial, id))
+
+mydb.commit()
+
+mycursor.execute("SELECT * FROM TB_ALUNOS")
+meuresultado = mycursor.fetchall()
+
+for item in meuresultado:
+    print(item)
+
+mycursor.close()
+mydb.close()
+EOF
+
+python arquivo.py
